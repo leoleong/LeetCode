781284b614cda35ec1a9c912bc2e5aa3e5d2c35b
leoleong <renzhejiajia@qq.com>
2014-08-04 13:53:07 +0800

    Update 'Letter Combinations of a Phone Number'

--- src/exhaustion/LetterCombinationsofaPhoneNumber.java
+++ src/exhaustion/LetterCombinationsofaPhoneNumber.java
@@ -1,3 +1,14 @@
+/**
+ * Problem:
+ * Given a digit string, return all possible letter combinations that the number could represent.
+ * A mapping of digit to letters (just like on the telephone buttons) is given below.
+ * 
+ * Input:Digit string "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 
+ * Note:
+ * Although the above answer is in lexicographical order, your answer could be in any order you want.
+ */
 package exhaustion;
 
 import java.util.ArrayList;
@@ -36,6 +47,7 @@ public class LetterCombinationsofaPhoneNumber {
 			result.add(path.toString());
 			return;
 		}
+		
 		int index = digits.charAt(start) - '0';
 		for (int i = 0; i < map[index].length(); i++) {
 			path.append(map[index].charAt(i));

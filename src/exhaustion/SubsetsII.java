22e3aeb36b55c2cd7a7901636305b2768569c741
leoleong <renzhejiajia@qq.com>
2014-08-04 11:17:08 +0800

    Update 'SubsetsII'

--- src/exhaustion/SubsetsII.java
+++ src/exhaustion/SubsetsII.java
@@ -1,3 +1,22 @@
+/**
+ * Problems:
+ * Given a collection of integers that might contain duplicates, S, return all possible subsets.
+ * 
+ * Note:
+ * 1. Elements in a subset must be in non-descending order.
+ * 2. The solution set must not contain duplicate subsets.
+ * 
+ * For example,
+ * If S = [1,2,2], a solution is:
+ * [
+ * 	[2],
+ * 	[1],
+ * 	[1,2,2],
+ * 	[2,2],
+ * 	[1,2],
+ * 	[]
+ * ]
+ */
 package exhaustion;
 
 import java.util.ArrayList;

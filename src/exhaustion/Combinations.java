d16331337b4738510ac5cb56f0b871d5cf6bc650
leoleong <renzhejiajia@qq.com>
2014-08-04 13:42:58 +0800

    Update 'Combinations'

--- src/exhaustion/Combinations.java
+++ src/exhaustion/Combinations.java
@@ -1,3 +1,18 @@
+/**
+ * Problem:
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ * 
+ * For example,
+ * If n = 4 and k = 2, a solution is:
+ * [
+ * 	[2,4],
+ * 	[3,4],
+ * 	[2,3],
+ * 	[1,2],
+ * 	[1,3],
+ * 	[1,4],
+ * ]
+ */
 package exhaustion;
 
 import java.util.ArrayList;
@@ -30,6 +45,7 @@ public class Combinations {
 			result.add(new ArrayList<Integer>(path));
 			return;
 		}
+		
 		for (int i = start; i <= n; i++) {
 			path.add(i);
 			dfs(result, path, i + 1, n, k);

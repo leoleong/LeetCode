9e835d97a72b12691f98a30c16c3ee171189aee3
leoleong <renzhejiajia@qq.com>
2014-08-05 10:39:35 +0800

    Update 'Restore IP Address'

--- src/dfs/RestoreIPAddresses.java
+++ src/dfs/RestoreIPAddresses.java
@@ -1,3 +1,11 @@
+/**
+ * Problem:
+ * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ * 
+ * For example:
+ * Given "25525511135",
+ * return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+ */
 package dfs;
 
 import java.util.ArrayList;
@@ -5,53 +13,54 @@ import java.util.List;
 
 public class RestoreIPAddresses {
 
-	private static int index;
-
 	public static void main(String[] args) {
 
 		String s = "1111";
 
 		List<String> result = restoreIpAddresses(s);
 
-		for (String str : result) {
-			System.out.println(str);
-		}
+		System.out.println(result);
 	}
 
 	public static List<String> restoreIpAddresses(String s) {
 
 		List<String> result = new ArrayList<String>();
-		String[] path = new String[4];
-		index = 0;
+		List<String> path = new ArrayList<String>();
 
 		dfs(result, path, s, 0);
 
 		return result;
 	}
 
-	private static void dfs(List<String> result, String[] path, String s, int start) {
+	private static void dfs(List<String> result, List<String> path, String s,
+			int start) {
 
-		int N = s.length();
-		if (start == N && index==4) {
-			result.add(path[0] + "." + path[1] + "." + path[2] + "." + path[3]);
+		if (path.size() == 4 && start == s.length()) {
+			result.add(path.get(0) + "." + path.get(1) + "." + path.get(2)
+					+ "." + path.get(3));
 		}
-		for (int i = start; i < N; i++) {
-			if (index == 4) {
-				return;
-			}
+
+		if (path.size() >= 4) {
+			return;
+		}
+
+		for (int i = start; i < s.length(); i++) {
 			if (isValid(s, start, i)) {
-				path[index++] = s.substring(start, i + 1);
+				path.add(s.substring(start, i + 1));
 				dfs(result, path, s, i + 1);
-				index--;
+				path.remove(path.size() - 1);
 			}
 		}
 	}
 
 	private static boolean isValid(String s, int start, int end) {
 
-		if (start == end || (end - start < 3 && s.charAt(start) != '0' && Integer.parseInt(s.substring(start, end + 1)) <= 255)) {
+		if (start == end
+				|| (end - start < 3 && s.charAt(start) != '0' && Integer
+						.parseInt(s.substring(start, end + 1)) <= 255)) {
 			return true;
 		}
+
 		return false;
 	}
 }

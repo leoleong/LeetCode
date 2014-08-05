0e0aa25a44c86f5f69baac4efdaadd01d750841e
leoleong <renzhejiajia@qq.com>
2014-08-05 09:54:22 +0800

    Update 'Combination Sum'

--- src/dfs/CombinationSum.java
+++ src/dfs/CombinationSum.java
@@ -1,3 +1,19 @@
+/**
+ * Problem:
+ * Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+ * The same repeated number may be chosen from C unlimited number of times.
+ * 
+ * Note:
+ * All numbers (including target) will be positive integers.
+ * Elements in a combination (a1, a2, ¡­ , ak) must be in non-descending order. (ie, a1 ¡Ü a2 ¡Ü ¡­ ¡Ü ak).
+ * The solution set must not contain duplicate combinations.
+ * 
+ * For example, given candidate set 2,3,6,7 and target 7, 
+ * A solution set is: 
+ * [7] 
+ * [2, 2, 3] 
+ * 
+ */
 package dfs;
 
 import java.util.ArrayList;
@@ -18,7 +34,8 @@ public class CombinationSum {
 		}
 	}
 
-	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+	public static List<List<Integer>> combinationSum(int[] candidates,
+			int target) {
 
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		List<Integer> path = new ArrayList<Integer>();
@@ -29,13 +46,14 @@ public class CombinationSum {
 		return result;
 	}
 
-	private static void dfs(List<List<Integer>> result, List<Integer> path, int[] candidates, int target, int start) {
+	private static void dfs(List<List<Integer>> result, List<Integer> path,
+			int[] candidates, int target, int start) {
 
 		if (target == 0) {
 			result.add(new ArrayList<Integer>(path));
 			return;
 		}
-		
+
 		for (int i = start; i < candidates.length; i++) {
 			if (target < candidates[i]) {
 				return;

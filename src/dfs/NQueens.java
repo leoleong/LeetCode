1f1b78f6e3ccb2b5ba14c36048f630069f506f70
leoleong <renzhejiajia@qq.com>
2014-08-06 22:54:17 +0800

    Update 'N-Queens'

--- src/dfs/NQueens.java
+++ src/dfs/NQueens.java
@@ -1,3 +1,23 @@
+/**
+ * Problem:
+ * The n-queens puzzle is the problem of placing n queens on an n¡Án chessboard such that no two queens attack each other.
+ * Given an integer n, return all distinct solutions to the n-queens puzzle.
+ * Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+ * 
+ * For example,
+ * There exist two distinct solutions to the 4-queens puzzle:
+ * [
+ * 	[".Q..",  // Solution 1
+ * 	 "...Q",
+ * 	 "Q...",
+ * 	  "..Q."],
+ * 
+ * 	["..Q.",  // Solution 2
+ * 	 "Q...",
+ *   "...Q",
+ *   ".Q.."]
+ * ]
+ */
 package dfs;
 
 import java.util.ArrayList;
@@ -30,7 +50,7 @@ public class NQueens {
 	private static void dfs(List<String[]> result, int[] path, int row) {
 
 		int N = path.length;
-		// convergence condition
+		
 		if (row == N) {
 			String[] strs = new String[N];
 			for (int i = 0; i < N; i++) {
@@ -47,7 +67,7 @@ public class NQueens {
 			result.add(strs);
 			return;
 		}
-		// normal condition
+		
 		for (int j = 0; j < N; j++) {
 			if (isValid(path, row, j)) {
 				path[row] = j;
@@ -59,7 +79,8 @@ public class NQueens {
 	private static boolean isValid(int[] path, int row, int column) {
 
 		for (int i = 0; i < row; i++) {
-			if (column == path[i] || Math.abs(column - path[i]) == Math.abs(row - i)) {
+			if (column == path[i]
+					|| Math.abs(column - path[i]) == Math.abs(row - i)) {
 				return false;
 			}
 		}

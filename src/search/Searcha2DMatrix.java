27799576fdeef916b6c6b20e5894f0b9a7c9f0fd
leoleong <renzhejiajia@qq.com>
2014-08-07 20:04:46 +0800

    Update 'Search a 2D Matrix'. - Add problem description.

--- src/search/Searcha2DMatrix.java
+++ src/search/Searcha2DMatrix.java
@@ -1,16 +1,30 @@
+/**
+ * Problem:
+ * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+ * 	- Integers in each row are sorted from left to right.
+ * 	- The first integer of each row is greater than the last integer of the previous row.
+ * 
+ * For example,
+ * Consider the following matrix:
+ * [
+ * 	[1,   3,  5,  7],
+ * 	[10, 11, 16, 20],
+ * 	[23, 30, 34, 50]
+ * ]
+ * Given target = 3, return true.
+ */
 package search;
 
 public class Searcha2DMatrix {
 
 	public static void main(String[] args) {
 
-		int[][] matrix = new int[][] { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 50 } };
+		int[][] matrix = new int[][] { { 1, 3, 5, 7 }, { 10, 11, 16, 20 },
+				{ 23, 30, 34, 50 } };
 		boolean result = searchMatrix(matrix, 0);
 		System.out.println(result);
 	}
 
-	// ----------------------------------------------------------------
-	// once binary-search
 	public static boolean searchMatrix(int[][] matrix, int target) {
 
 		int row = matrix.length;
@@ -31,50 +45,5 @@ public class Searcha2DMatrix {
 		}
 
 		return false;
-	}
-
-	// ----------------------------------------------------------------
-	// twice binary-search
-	public static boolean searchMatrix_1(int[][] matrix, int target) {
-
-		int row = matrix.length - 1;
-		int column = matrix[0].length - 1;
-
-		if (matrix[row][column] < target) {
-			return false;
-		} else {
-			int start = 0;
-			int end = row;
-			int mid;
-			int index = -1;
-
-			while (start <= end) {
-				mid = (start + end) / 2;
-				if (matrix[mid][column] < target) {
-					start = mid + 1;
-					index = start;
-				} else if (matrix[mid][column] > target) {
-					end = mid - 1;
-					index = mid;
-				} else {
-					return true;
-				}
-			}
-
-			start = 0;
-			end = column;
-			while (start <= end) {
-				mid = (start + end) / 2;
-				if (matrix[index][mid] > target) {
-					end = mid - 1;
-				} else if (matrix[index][mid] < target) {
-					start = mid + 1;
-				} else {
-					return true;
-				}
-			}
-
-			return false;
-		}
 	}
 }

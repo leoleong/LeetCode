4289cb7c81a8f5362fb2d941b2b6e61edceaee89
leoleong <renzhejiajia@qq.com>
2014-08-07 20:05:51 +0800

    Update 'Search Insert Position'. - Add problem description.

--- src/search/SearchInsertPosition.java
+++ src/search/SearchInsertPosition.java
@@ -1,11 +1,16 @@
+/**
+ * Problem:
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * You may assume no duplicates in the array.
+ * 
+ * Here are few examples.
+ * [1,3,5,6], 5 → 2
+ * [1,3,5,6], 2 → 1
+ * [1,3,5,6], 7 → 4
+ * [1,3,5,6], 0 → 0
+ */
 package search;
 
-/**
- * author : leo
- * date : 2014-5-16
- * comment : 
- * 二分查找变种，添加额外记录量
- */
 public class SearchInsertPosition {
 
 	public static void main(String[] args) {
@@ -19,24 +24,24 @@ public class SearchInsertPosition {
 
 		int start = 0;
 		int end = A.length - 1;
-		int index = 0;
+		int pos = 0;
 		int mid;
 
 		while (start <= end) {
 			mid = (start + end) / 2;
 			if (A[mid] < target) {
 				start = mid + 1;
-				index = start;
+				pos = start;
 			} else if (A[mid] > target) {
 				end = mid - 1;
 				// a little trick here
-				index = mid;
+				pos = mid;
 			} else {
-				index = mid;
+				pos = mid;
 				break;
 			}
 		}
 
-		return index;
+		return pos;
 	}
 }

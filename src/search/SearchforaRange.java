fbfc5644f801af01952dfa57ba466a249b6fa298
leoleong <renzhejiajia@qq.com>
2014-08-07 20:05:19 +0800

    Update 'Search for a Range'. - Add problem description.

--- src/search/SearchforaRange.java
+++ src/search/SearchforaRange.java
@@ -1,11 +1,15 @@
+/**
+ * Problem:
+ * Given a sorted array of integers, find the starting and ending position of a given target value.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * If the target is not found in the array, return [-1, -1].
+ * 
+ * For example,
+ * Given [5, 7, 7, 8, 8, 10] and target value 8,
+ * return [3, 4].
+ */
 package search;
 
-/**
- * author : leo
- * date : 2014-5-16
- * comment : 
- * 二分查找变种
- */
 public class SearchforaRange {
 
 	public static void main(String[] args) {

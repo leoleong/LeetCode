30f229632488aeab2699014bbf77f0503ac943a1
leoleong <renzhejiajia@qq.com>
2014-08-09 16:32:00 +0800

    Update 'Remove Duplicates from Sorted Array II'. - Add problem description. - Modify code-style, add braces for single statement. - Modify corner condition.

--- src/array/RemoveDuplicatesfromSortedArrayII.java
+++ src/array/RemoveDuplicatesfromSortedArrayII.java
@@ -1,3 +1,12 @@
+/**
+ * Problem:
+ * Follow up for "Remove Duplicates":
+ * What if duplicates are allowed at most twice?
+ * 
+ * For example,
+ * Given sorted array A = [1,1,1,2,2,3],
+ * Your function should return length = 5, and A is now [1,1,2,2,3].
+ */
 package array;
 
 public class RemoveDuplicatesfromSortedArrayII {
@@ -11,16 +20,17 @@ public class RemoveDuplicatesfromSortedArrayII {
 
 	public static int removeDuplicates(int[] A) {
 
-		if (A.length <= 2)
+		if (A == null || A.length == 0) {
 			return A.length;
+		}
 
 		int index = 0;
 		int count = 1;
 		for (int i = 1; i < A.length; i++) {
-			if (A[i] != A[i - 1]) {
+			if (A[index] != A[i]) {
 				A[++index] = A[i];
 				count = 1;
-			} else if (A[i] == A[i - 1] && count < 2) {
+			} else if (A[index] == A[i] && count < 2) {
 				A[++index] = A[i];
 				count++;
 			} else {

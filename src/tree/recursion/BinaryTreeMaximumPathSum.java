f531a3f1aad65db2ef9575cdf7982b3deb12b90f
leoleong <renzhejiajia@qq.com>
2014-08-11 21:24:24 +0800

    Update 'Binary Tree Maximum Path Sum'. - Add problem description. - Classify the method as Divide&Conquer.

--- src/tree/recursion/BinaryTreeMaximumPathSum.java
+++ src/tree/recursion/BinaryTreeMaximumPathSum.java
@@ -1,3 +1,15 @@
+/**
+ * Problem:
+ * Given a binary tree, find the maximum path sum.
+ * The path may start and end at any node in the tree.
+ * 
+ * For example:
+ * Given the below binary tree,
+ * 		1
+ * 	   / \
+ *    2   3
+ * Return 6.
+ */
 package tree.recursion;
 
 public class BinaryTreeMaximumPathSum {
@@ -11,25 +23,27 @@ public class BinaryTreeMaximumPathSum {
 	public static int maxPathSum(TreeNode root) {
 
 		max = Integer.MIN_VALUE;
-
 		dfs(root);
-
+		
 		return max;
 	}
 
 	private static int dfs(TreeNode root) {
 
-		// terminal condition
 		if (root == null) {
 			return 0;
 		}
 		
+		// divide
 		int left = dfs(root.left);
 		int right = dfs(root.right);
-		int sum = root.val + (left > 0 ? left : 0) + (right > 0 ? right : 0);
-		max = Math.max(max, sum);
+		
+		// conquer
+		int tatalSum = root.val + (left > 0 ? left : 0) + (right > 0 ? right : 0);
+		max = Math.max(max, tatalSum);
+		int partialSum = Math.max(left, right) > 0 ? Math.max(left, right) + root.val : root.val;
 
-		return Math.max(left, right) > 0 ? Math.max(left, right) + root.val : root.val;
+		return partialSum;
 	}
 
 	// Definition for binary tree

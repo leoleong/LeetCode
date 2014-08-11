13e1fcb3d578fbae99fcaa40337309f65ebae5dc
leoleong <renzhejiajia@qq.com>
2014-08-11 18:41:22 +0800

    Update 'Binary Tree Level Order Traversal'. - Add problem description. - Add bfs method.

--- src/tree/traversal/BinaryTreeLevelOrderTraversal.java
+++ src/tree/traversal/BinaryTreeLevelOrderTraversal.java
@@ -1,7 +1,27 @@
+/**
+ * Problem:
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ * 
+ * For example:
+ * Given binary tree {3,9,20,#,#,15,7},
+ * 		3
+ *	   / \
+ *    9  20
+ *       / \
+ *      15  7
+ * return its level order traversal as:
+ * [
+ * 	[3],
+ * 	[9,20],
+ * 	[15,7]
+ * ]
+ */
 package tree.traversal;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinaryTreeLevelOrderTraversal {
 
@@ -13,16 +33,19 @@ public class BinaryTreeLevelOrderTraversal {
 
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 
-		dfs(result, 1, root);
+//		dfs(result, 1, root);
+		bfs(result, root);
 
 		return result;
 	}
 
+	// Method 1: DFS
 	private static void dfs(List<List<Integer>> result, int level, TreeNode node) {
 
 		if (node == null) {
 			return;
 		}
+		
 		if (result.size() < level) {
 			result.add(new ArrayList<Integer>());
 		}
@@ -31,8 +54,34 @@ public class BinaryTreeLevelOrderTraversal {
 		dfs(result, level + 1, node.right);
 	}
 
+	// Method 2: BFS
+	private static void bfs(List<List<Integer>> result, TreeNode root) {
+
+		if (root == null) {
+			return;
+		}
+
+		Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			List<Integer> level = new ArrayList<Integer>();
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				level.add(node.val);
+				if (node.left != null) {
+					queue.offer(node.left);
+				}
+				if (node.right != null) {
+					queue.offer(node.right);
+				}
+			}
+			result.add(level);
+		}
+	}
+
 	// Definition for binary tree
-	private class TreeNode {
+	public class TreeNode {
 
 		int val;
 		TreeNode left;

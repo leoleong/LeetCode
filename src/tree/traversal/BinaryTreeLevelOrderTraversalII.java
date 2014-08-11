ab3f0b95b395e784494133fadbaa274fdea82db8
leoleong <renzhejiajia@qq.com>
2014-08-11 18:41:35 +0800

    Update 'Binary Tree Level Order Traversal II'. - Add problem description. - Add bfs method.

--- src/tree/traversal/BinaryTreeLevelOrderTraversalII.java
+++ src/tree/traversal/BinaryTreeLevelOrderTraversalII.java
@@ -1,8 +1,28 @@
+/**
+ * Problem:
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+ * 
+ * For example:
+ * Given binary tree {3,9,20,#,#,15,7},
+ * 		3
+ *	   / \
+ *    9  20
+ *       / \
+ *      15  7
+ * return its bottom-up level order traversal as:
+ * [
+ * 	[15,7],
+ * 	[9,20],
+ * 	[3]
+ * ]
+ */
 package tree.traversal;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Queue;
 
 public class BinaryTreeLevelOrderTraversalII {
 
@@ -14,17 +34,20 @@ public class BinaryTreeLevelOrderTraversalII {
 
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 
-		dfs(result, 1, root);
+//		dfs(result, 1, root);
+		bfs(result, root);
 		Collections.reverse(result);
 
 		return result;
 	}
 
+	// Method 1: DFS
 	private static void dfs(List<List<Integer>> result, int level, TreeNode node) {
 
 		if (node == null) {
 			return;
 		}
+
 		if (result.size() < level) {
 			result.add(new ArrayList<Integer>());
 		}
@@ -33,8 +56,34 @@ public class BinaryTreeLevelOrderTraversalII {
 		dfs(result, level + 1, node.right);
 	}
 
+	// Method 2: BFS
+	private static void bfs(List<List<Integer>> result, TreeNode root) {
+
+		if (root == null) {
+			return;
+		}
+
+		Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			List<Integer> level = new ArrayList<Integer>();
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				level.add(node.val);
+				if (node.left != null) {
+					queue.offer(node.left);
+				}
+				if (node.right != null) {
+					queue.offer(node.right);
+				}
+			}
+			result.add(level);
+		}
+	}
+
 	// Definition for binary tree
-	private class TreeNode {
+	public class TreeNode {
 
 		int val;
 		TreeNode left;

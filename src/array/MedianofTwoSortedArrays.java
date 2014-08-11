042245bca5e8c96cf21bcff0e5db46b1acf35262
leoleong <renzhejiajia@qq.com>
2014-08-11 08:57:02 +0800

    Update 'Median of Two Sorted Arrays'. - Add problem description. - Change from O(m+n) to O(log(m+n)).

--- src/array/MedianofTwoSortedArrays.java
+++ src/array/MedianofTwoSortedArrays.java
@@ -1,3 +1,8 @@
+/**
+ * Problem:
+ * There are two sorted arrays A and B of size m and n respectively. 
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ */
 package array;
 
 public class MedianofTwoSortedArrays {
@@ -10,32 +15,38 @@ public class MedianofTwoSortedArrays {
 		System.out.println(result);
 	}
 
-	public static double findMedianSortedArrays(int A[], int B[]) {
+	public static double findMedianSortedArrays(int[] A, int[] B) {
 
-		int[] C = new int[A.length + B.length];
-		int i = 0, j = 0, k = 0;
-		
-		while (i < A.length && j < B.length) {
-			if (A[i] <= B[j]) {
-				C[k++] = A[i++];
-			} else {
-				C[k++] = B[j++];
-			}
-		}
-		while (i < A.length) {
-			C[k++] = A[i++];
-		}
-		while (j < B.length) {
-			C[k++] = B[j++];
-		}
-		
-		double median;
-		if (C.length % 2 == 1) {
-			median = C[C.length / 2];
+		int length = A.length + B.length;
+		if (length % 2 == 1) {
+			return helper(A, 0, B, 0, length / 2 + 1);
 		} else {
-			median = (double) (C[C.length / 2] + C[C.length / 2 - 1]) / 2;
+			return (helper(A, 0, B, 0, length / 2) + helper(A, 0, B, 0,
+					length / 2 + 1)) / 2.0;
 		}
-		
-		return median;
+	}
+
+	private static int helper(int[] A, int aStart, int[] B, int bStart, int k) {
+
+		if (aStart >= A.length) {
+			return B[bStart + k - 1];
+		}
+		if (bStart >= B.length) {
+			return A[aStart + k - 1];
+		}
+
+		if (k == 1) {
+			return Math.min(A[aStart], B[bStart]);
+		}
+
+		int aKey = aStart + k / 2 - 1 < A.length ? A[aStart + k / 2 - 1]
+				: Integer.MAX_VALUE;
+		int bKey = bStart + k / 2 - 1 < B.length ? B[bStart + k / 2 - 1]
+				: Integer.MAX_VALUE;
+		if (aKey < bKey) {
+			return helper(A, aStart + k / 2, B, bStart, k - k / 2);
+		} else {
+			return helper(A, aStart, B, bStart + k / 2, k - k / 2);
+		}
 	}
 }

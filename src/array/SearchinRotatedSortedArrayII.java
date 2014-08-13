3ea16f197b34aa82ad4003f461becae01b46abe9
leoleong <renzhejiajia@qq.com>
2014-08-13 23:06:56 +0800

    Update 'Search in Rotated Sorted Array II'. - Change 'mid = (start + end) / 2' to 'mid = start + (end - start) / 2'.

--- src/array/SearchinRotatedSortedArrayII.java
+++ src/array/SearchinRotatedSortedArrayII.java
@@ -22,7 +22,7 @@ public class SearchinRotatedSortedArrayII {
 		int end = A.length - 1;
 
 		while (start <= end) {
-			int mid = (start + end) / 2;
+			int mid = start + (end - start) / 2;
 			if (target == A[mid]) {
 				return true;
 			}

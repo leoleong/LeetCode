d041eb1460b32e6744d88dc7559c92a4c73118d6
leoleong <renzhejiajia@qq.com>
2014-08-13 11:49:27 +0800

    Update 'Sudoku Solver'. - Add problem description.

--- src/dfs/SudokuSolver.java
+++ src/dfs/SudokuSolver.java
@@ -1,18 +1,24 @@
+/**
+ * Problem:
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ * Empty cells are indicated by the character '.'.
+ * You may assume that there will be only one unique solution.
+ */
 package dfs;
 
 public class SudokuSolver {
 
 	public static void main(String[] args) {
 
-		char[][] board = { { '5', '3', '.', '.', '7', '.', '.', '.', '.' }, 
-							{ '6', '.', '.', '1', '9', '5', '.', '.', '.' },
-							{ '.', '9', '8', '.', '.', '.', '.', '6', '.' }, 
-							{ '8', '.', '.', '.', '6', '.', '.', '.', '3' },
-							{ '4', '.', '.', '8', '.', '3', '.', '.', '1' }, 
-							{ '7', '.', '.', '.', '2', '.', '.', '.', '6' },
-							{ '.', '6', '.', '.', '.', '.', '2', '8', '.' }, 
-							{ '.', '.', '.', '4', '1', '9', '.', '.', '5' },
-							{ '.', '.', '.', '.', '8', '.', '.', '7', '9' }, };
+		char[][] board = { { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+				{ '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+				{ '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+				{ '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+				{ '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+				{ '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+				{ '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+				{ '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+				{ '.', '.', '.', '.', '8', '.', '.', '7', '9' }, };
 
 		solveSudoku(board);
 
@@ -35,12 +41,13 @@ public class SudokuSolver {
 		if (row > 8) {
 			return true;
 		}
+		
 		// normal condition
 		if (board[row][column] == '.') {
 			for (int k = 1; k <= 9; k++) {
 				board[row][column] = (char) ('0' + k);
 				if (isValid(board, row, column)) {
-					if(dfs(board, row, column + 1)){
+					if (dfs(board, row, column + 1)) {
 						return true;
 					}
 				}
@@ -49,6 +56,7 @@ public class SudokuSolver {
 		} else {
 			return dfs(board, row, column + 1);
 		}
+
 		return false;
 	}
 
@@ -66,11 +74,13 @@ public class SudokuSolver {
 		}
 		for (int i = row / 3 * 3; i < row / 3 * 3 + 3; i++) {
 			for (int j = column / 3 * 3; j < column / 3 * 3 + 3; j++) {
-				if (board[i][j] == board[row][column] && i != row && j != column) {
+				if (board[i][j] == board[row][column] && i != row
+						&& j != column) {
 					return false;
 				}
 			}
 		}
+
 		return true;
 	}
 }

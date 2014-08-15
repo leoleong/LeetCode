aaf47b48d61f210ea04d1120f41adf850697fffc
leoleong <renzhejiajia@qq.com>
2014-08-15 17:08:44 +0800

    Update 'Unique Binary Search Trees'. - Add problem description. - DP.

--- src/tree/search/UniqueBinarySearchTrees.java
+++ src/tree/search/UniqueBinarySearchTrees.java
@@ -1,3 +1,15 @@
+/**
+ * Problem:
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+ * 
+ * For example,
+ * Given n = 3, there are a total of 5 unique BST's.
+ * 	 1         3     3      2      1
+ *    \       /     /      / \      \
+ *     3     2     1      1   3      2
+ *    /     /       \                 \
+ *   2     1         2                 3
+ */
 package tree.search;
 
 public class UniqueBinarySearchTrees {
@@ -5,7 +17,7 @@ public class UniqueBinarySearchTrees {
 	public static void main(String[] args) {
 
 		int result = numTrees(3);
-		
+
 		System.out.println(result);
 	}
 
@@ -14,12 +26,12 @@ public class UniqueBinarySearchTrees {
 		if (n < 2) {
 			return 1;
 		}
-		
+
 		int[] dp = new int[n + 1];
 		dp[0] = dp[1] = 1;
 		for (int i = 2; i < dp.length; i++) {
-			for (int j = 1; j <= i; j++) {
-				dp[i] += dp[i - j] * dp[j - 1];
+			for (int j = 0; j < i; j++) {
+				dp[i] += dp[j] * dp[i - 1 - j];
 			}
 		}
 

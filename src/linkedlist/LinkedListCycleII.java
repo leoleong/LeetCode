1ddfda0bf4a23d86b8cd883cb058091196411bf8
leoleong <renzhejiajia@qq.com>
2014-08-16 21:09:15 +0800

    Update 'Linked List Cycle II'. - Add problem description. - Use 'two-pointer'.

--- src/linkedlist/LinkedListCycleII.java
+++ src/linkedlist/LinkedListCycleII.java
@@ -1,3 +1,10 @@
+/**
+ * Problem:
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ * 
+ * Follow up:
+ * Can you solve it without using extra space?
+ */
 package linkedlist;
 
 public class LinkedListCycleII {
@@ -34,7 +41,6 @@ public class LinkedListCycleII {
 
 		ListNode(int x) {
 			val = x;
-			next = null;
 		}
 	}
 }

6a92b23fabb12b33e7ed4ad0cd25375a56674cf3
leoleong <renzhejiajia@qq.com>
2014-08-16 12:15:47 +0800

    Update 'Sum Root to Leaf Numbers'. - Add problem description. - Divide & Conquer. - Params in Recursion are used to pass states.

--- src/tree/recursion/SumRoottoLeafNumbers.java
+++ src/tree/recursion/SumRoottoLeafNumbers.java
@@ -1,3 +1,17 @@
+/**
+ * Problem:
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+ * An example is the root-to-leaf path 1->2->3 which represents the number 123.
+ * Find the total sum of all root-to-leaf numbers.
+ * 
+ * For example,
+ * 		1
+ * 	   / \
+ * 	  2	  3
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Return the sum = 12 + 13 = 25.
+ */
 package tree.recursion;
 
 public class SumRoottoLeafNumbers {
@@ -8,23 +22,28 @@ public class SumRoottoLeafNumbers {
 
 	public static int sumNumbers(TreeNode root) {
 
-		return dfs(root, 0);
+		return sumNumbers(root, 0);
 	}
 
-	private static int dfs(TreeNode root, int sum) {
+	private static int sumNumbers(TreeNode root, int sum) {
 
 		if (root == null) {
 			return 0;
 		}
+
+		sum = sum * 10 + root.val;
 		if (root.left == null && root.right == null) {
-			return sum * 10 + root.val;
+			return sum;
 		}
 
-		return dfs(root.left, sum * 10 + root.val) + dfs(root.right, sum * 10 + root.val);
+		int left = sumNumbers(root.left, sum);
+		int right = sumNumbers(root.right, sum);
+		
+		return left + right;
 	}
 
 	// Definition for binary tree
-	private static class TreeNode {
+	public static class TreeNode {
 		int val;
 		TreeNode left;
 		TreeNode right;

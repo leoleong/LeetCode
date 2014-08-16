840b8d5b3c95856c4e869e838a060bfc8a4c6e8e
leoleong <renzhejiajia@qq.com>
2014-08-16 18:17:23 +0800

    Update 'Partition List'. - Add problem description. - Use dummy node while head is infinite.

--- src/linkedlist/PartitionList.java
+++ src/linkedlist/PartitionList.java
@@ -1,3 +1,13 @@
+/**
+ * Problem:
+ * Given a linked list and a value x, partition it such that 
+ * all nodes less than x come before nodes greater than or equal to x.
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+ * 
+ * For example,
+ * Given 1->4->3->2->5->2 and x = 3,
+ * return 1->2->2->3->4->5.
+ */
 package linkedlist;
 
 public class PartitionList {
@@ -11,9 +21,9 @@ public class PartitionList {
 			prev.next = new ListNode(i);
 			prev = prev.next;
 		}
-		
+
 		ListNode node = partition(dummy.next, 3);
-		
+
 		while (node != null) {
 			System.out.print(node.val + " ");
 			node = node.next;
@@ -22,25 +32,25 @@ public class PartitionList {
 
 	public static ListNode partition(ListNode head, int x) {
 
-		ListNode dummy1 = new ListNode(-1);
-		ListNode dummy2 = new ListNode(-1);
-		ListNode cur1 = dummy1;
-		ListNode cur2 = dummy2;
-		
+		ListNode leftDummy = new ListNode(-1);
+		ListNode rightDummy = new ListNode(-1);
+		ListNode left = leftDummy;
+		ListNode right = rightDummy;
+
 		while (head != null) {
 			if (head.val < x) {
-				cur1.next = head;
-				cur1 = cur1.next;
+				left.next = head;
+				left = left.next;
 			} else {
-				cur2.next = head;
-				cur2 = cur2.next;
+				right.next = head;
+				right = right.next;
 			}
 			head = head.next;
 		}
-		cur1.next = dummy2.next;
-		cur2.next = null;
-		
-		return dummy1.next;
+		left.next = rightDummy.next;
+		right.next = null;
+
+		return leftDummy.next;
 	}
 
 	// Definition for singly-linked list.
@@ -50,7 +60,6 @@ public class PartitionList {
 
 		ListNode(int x) {
 			val = x;
-			next = null;
 		}
 	}
 }

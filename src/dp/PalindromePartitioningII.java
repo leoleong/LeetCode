08a0ec8cbb31f365aa2723eb1d0728b10bec3260
leoleong <renzhejiajia@qq.com>
2014-08-17 22:45:21 +0800

    Update 'Palindrome Partitioning II'. - Add problem description. - Sequence DP.

--- src/dp/PalindromePartitioningII.java
+++ src/dp/PalindromePartitioningII.java
@@ -1,11 +1,13 @@
+/**
+ * Problem:
+ * Given a string s, partition s such that every substring of the partition is a palindrome.
+ * Return the minimum cuts needed for a palindrome partitioning of s.
+ * 
+ * For example, given s = "aab",
+ * Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+ */
 package dp;
 
-/**
- * author : leo
- * date : 2014-5-22
- * comment : 
- * 增加extra元素,化简情况,最后需要去除
- */
 public class PalindromePartitioningII {
 
 	public static void main(String[] args) {
@@ -17,18 +19,46 @@ public class PalindromePartitioningII {
 		System.out.println(result);
 	}
 
+	// Method 1: Sequence DP(from start)
 	public static int minCut(String s) {
 
+		if (s.length() <= 1) {
+			return 0;
+		}
+
+		boolean[][] isPalindrome = new boolean[s.length() + 1][s.length() + 1];
+		int[] dp = new int[s.length() + 1];
+		dp[0] = -1;
+
+		for (int i = 1; i < dp.length; i++) {
+			dp[i] = i - 1;
+			for (int j = 1; j <= i; j++) {
+				// little trick here
+				if (s.charAt(i - 1) == s.charAt(j - 1)
+						&& (i - j < 2 || isPalindrome[j + 1][i - 1])) {
+					isPalindrome[j][i] = true;
+					dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+				}
+			}
+		}
+
+		return dp[s.length()];
+	}
+
+	// Method 2: Sequence DP(from end)
+	public int minCutFromBack(String s) {
+
 		int length = s.length();
-		// add extra element
 		int[] dp = new int[length + 1];
-		boolean[][] matrix = new boolean[length][length];
+		boolean[][] isPalindrome = new boolean[length][length];
 
 		for (int i = length; i >= 0; i--) {
 			dp[i] = length - 1 - i;
 			for (int j = i; j < length; j++) {
-				if (s.charAt(i) == s.charAt(j) && (j - i < 2 || matrix[i + 1][j - 1])) {
-					matrix[i][j] = true;
+				// little trick here
+				if (s.charAt(i) == s.charAt(j)
+						&& (j - i < 2 || isPalindrome[i + 1][j - 1])) {
+					isPalindrome[i][j] = true;
 					dp[i] = Math.min(dp[i], dp[j + 1] + 1);
 				}
 			}

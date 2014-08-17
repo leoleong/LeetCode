dbd921bbbe4557803cfa40d703bfa252640a2371
leoleong <renzhejiajia@qq.com>
2014-08-17 22:43:36 +0800

    Update 'Minimum Path Sum'. - Add problem description. - Matrix DP.

--- src/dp/MinimumPathSum.java
+++ src/dp/MinimumPathSum.java
@@ -1,3 +1,11 @@
+/**
+ * Problem:
+ * Given a m x n grid filled with non-negative numbers, 
+ * find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+ * 
+ * Note: 
+ * You can only move either down or right at any point in time.
+ */
 package dp;
 
 public class MinimumPathSum {
@@ -9,28 +17,29 @@ public class MinimumPathSum {
 	public static int minPathSum(int[][] grid) {
 
 		// special test case
-		if (grid.length == 0) {
+		if (grid.length == 0 || grid[0].length == 0) {
 			return 0;
 		}
-		
+
 		// functional test case
 		int row = grid.length;
 		int column = grid[0].length;
-		int[][] f = new int[row][column];
-		f[0][0] = grid[0][0];
+		int[][] dp = new int[row][column];
 
+		dp[0][0] = grid[0][0];
 		for (int i = 1; i < row; i++) {
-			f[i][0] = f[i - 1][0] + grid[i][0];
+			dp[i][0] = dp[i - 1][0] + grid[i][0];
 		}
 		for (int j = 1; j < column; j++) {
-			f[0][j] = f[0][j - 1] + grid[0][j];
+			dp[0][j] = dp[0][j - 1] + grid[0][j];
 		}
+
 		for (int i = 1; i < row; i++) {
 			for (int j = 1; j < column; j++) {
-				f[i][j] = Math.min(f[i][j - 1], f[i - 1][j]) + grid[i][j];
+				dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
 			}
 		}
 
-		return f[row - 1][column - 1];
+		return dp[row - 1][column - 1];
 	}
 }

0ebaa959e9bff26c7b4cf4b3dff6bd84d747ecab
leoleong <renzhejiajia@qq.com>
2014-08-17 22:44:21 +0800

    Update 'Unique Paths II'. - Add problem description. - Matrix DP.

--- src/dp/UniquePathsII.java
+++ src/dp/UniquePathsII.java
@@ -1,4 +1,22 @@
-package dfs;
+/**
+ * Problem:
+ * Follow up for "Unique Paths":
+ * Now consider if some obstacles are added to the grids. How many unique paths would there be?
+ * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ * 
+ * For example,
+ * There is one obstacle in the middle of a 3x3 grid as illustrated below.
+ * [
+ * 	[0,0,0],
+ * 	[0,1,0],
+ * 	[0,0,0],
+ * ]
+ * The total number of unique paths is 2.
+ * 
+ * Note: 
+ * m and n will be at most 100.
+ */
+package dp;
 
 public class UniquePathsII {
 
@@ -6,11 +24,12 @@ public class UniquePathsII {
 
 		int[][] obstacleGrid = new int[][] { { 0, 0 }, { 1, 0 } };
 
-		int result = uniquePathsWithObstacles_1(obstacleGrid);
+		int result = uniquePathsWithObstacles(obstacleGrid);
 
 		System.out.println(result);
 	}
 
+	// Method 1: Matrix DP
 	public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
 
 		int row = obstacleGrid.length;
@@ -23,6 +42,7 @@ public class UniquePathsII {
 		for (int j = 0; j < column && obstacleGrid[0][j] == 0; j++) {
 			dp[0][j] = 1;
 		}
+
 		for (int i = 1; i < row; i++) {
 			for (int j = 1; j < column; j++) {
 				if (obstacleGrid[i][j] != 1) {
@@ -34,7 +54,8 @@ public class UniquePathsII {
 		return dp[row - 1][column - 1];
 	}
 
-	public static int uniquePathsWithObstacles_1(int[][] obstacleGrid) {
+	// Method 2: Matrix DP + Rolling Array
+	public static int uniquePathsWithObstaclesWith1D(int[][] obstacleGrid) {
 
 		int row = obstacleGrid.length;
 		int column = obstacleGrid[0].length;
@@ -49,7 +70,8 @@ public class UniquePathsII {
 		dp[0] = 1;
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < column; j++) {
-				dp[j] = obstacleGrid[i][j] == 1 ? 0 : (j > 0 ? dp[j] + dp[j - 1] : dp[j]);
+				dp[j] = obstacleGrid[i][j] == 1 ? 0 : (j > 0 ? dp[j]
+						+ dp[j - 1] : dp[j]);
 			}
 		}
 

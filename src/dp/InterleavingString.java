44ce3456408271c0bfd460006a8b95ed882ea3de
leoleong <renzhejiajia@qq.com>
2014-08-17 23:01:28 +0800

    Update 'Interleaving String'. - Add problem description. - 2-Sequence DP.

--- src/dp/InterleavingString.java
+++ src/dp/InterleavingString.java
@@ -1,11 +1,16 @@
+/**
+ * Problem:
+ * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+ * 
+ * For example,
+ * Given:
+ * s1 = "aabcc",
+ * s2 = "dbbca",
+ * When s3 = "aadbbcbcac", return true.
+ * When s3 = "aadbbbaccc", return false.
+ */
 package dp;
 
-/**
- * author : leo
- * date : 2014-5-23
- * comment :
- * 注意长度不相等的情况 
- */
 public class InterleavingString {
 
 	public static void main(String[] args) {
@@ -21,28 +26,29 @@ public class InterleavingString {
 
 	public static boolean isInterleave(String s1, String s2, String s3) {
 
-		int row = s1.length() + 1;
-		int column = s2.length() + 1;
-		if (row + column - 2 != s3.length()) {
+		if (s1.length() + s2.length() != s3.length()) {
 			return false;
-		} else {
-			boolean[][] f = new boolean[row][column];
-			f[0][0] = true;
-
-			for (int i = 1; i < row; i++) {
-				f[i][0] = f[i - 1][0] && s3.charAt(i - 1) == s1.charAt(i - 1);
-			}
-			for (int j = 1; j < column; j++) {
-				f[0][j] = f[0][j - 1] && s3.charAt(j - 1) == s2.charAt(j - 1);
-			}
-			for (int i = 1; i < row; i++) {
-				for (int j = 1; j < column; j++) {
-					f[i][j] = (f[i - 1][j] && s3.charAt(i + j - 1) == s1.charAt(i - 1))
-							|| (f[i][j - 1] && s3.charAt(i + j - 1) == s2.charAt(j - 1));
-				}
-			}
-
-			return f[row - 1][column - 1];
 		}
+
+		boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+		dp[0][0] = true;
+
+		for (int i = 1; i < dp.length; i++) {
+			dp[i][0] = dp[i - 1][0] && s3.charAt(i - 1) == s1.charAt(i - 1);
+		}
+		for (int j = 1; j < dp[0].length; j++) {
+			dp[0][j] = dp[0][j - 1] && s3.charAt(j - 1) == s2.charAt(j - 1);
+		}
+
+		for (int i = 1; i < dp.length; i++) {
+			for (int j = 1; j < dp[0].length; j++) {
+				dp[i][j] = (dp[i - 1][j] && s3.charAt(i + j - 1) == s1
+						.charAt(i - 1))
+						|| (dp[i][j - 1] && s3.charAt(i + j - 1) == s2
+								.charAt(j - 1));
+			}
+		}
+
+		return dp[s1.length()][s2.length()];
 	}
 }

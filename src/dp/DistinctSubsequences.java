3b1c6cbd2cd84501515936da3c9c529516b7a607
leoleong <renzhejiajia@qq.com>
2014-08-18 11:47:53 +0800

    Update 'Distinct Subsequences'. - Problem description. - 2-Sequence DP.

--- src/dp/DistinctSubsequences.java
+++ src/dp/DistinctSubsequences.java
@@ -1,3 +1,14 @@
+/**
+ * Problem:
+ * Given a string S and a string T, count the number of distinct subsequences of T in S.
+ * A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) 
+ * of the characters without disturbing the relative positions of the remaining characters. 
+ * (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+ * 
+ * Here is an example:
+ * S = "rabbbit", T = "rabbit"
+ * Return 3.
+ */
 package dp;
 
 public class DistinctSubsequences {
@@ -14,23 +25,22 @@ public class DistinctSubsequences {
 
 	public static int numDistinct(String S, String T) {
 
-		int column = S.length() + 1;
-		int row = T.length() + 1;
-		int[][] dp = new int[row][column];
+		int[][] dp = new int[S.length() + 1][T.length() + 1];
 
-		for (int j = 0; j < column; j++) {
-			dp[0][j] = 1;
+		for (int i = 0; i < dp.length; i++) {
+			dp[i][0] = 1;
 		}
-		for (int i = 1; i < row; i++) {
-			for (int j = i; j < column; j++) {
-				if (T.charAt(i - 1) == S.charAt(j - 1)) {
-					dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1];
+
+		for (int j = 1; j < dp[0].length; j++) {
+			for (int i = 1; i < dp.length; i++) {
+				if (T.charAt(j - 1) == S.charAt(i - 1)) {
+					dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
 				} else {
-					dp[i][j] = dp[i][j - 1];
+					dp[i][j] = dp[i - 1][j];
 				}
 			}
 		}
 
-		return dp[row - 1][column - 1];
+		return dp[S.length()][T.length()];
 	}
 }

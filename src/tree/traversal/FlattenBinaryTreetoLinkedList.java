686295b62aafe48af462713c949402f177fd133f
leoleong <renzhejiajia@qq.com>
2014-08-23 11:18:44 +0800

    Update 'Flatten Binary Tree to Linked List'. - Better solution: Divide & Conquer.

--- src/tree/traversal/FlattenBinaryTreetoLinkedList.java
+++ src/tree/traversal/FlattenBinaryTreetoLinkedList.java
@@ -29,8 +29,14 @@ public class FlattenBinaryTreetoLinkedList {
 
 	public static void main(String[] args) {
 
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node2 = new TreeNode(2);
+		node1.left = node2;
+
+		flatten(node1);
 	}
 
+	// Method 1: Divide & Conquer
 	public static void flatten(TreeNode root) {
 
 		if (root == null) {

c184a69109a8ba6ae15c2250ac7f5d214aae15ec
leoleong <renzhejiajia@qq.com>
2014-08-24 21:07:25 +0800

    Update 'Palindrome Partitioning II'. - Modify initial condition.

--- src/dp/PalindromePartitioningII.java
+++ src/dp/PalindromePartitioningII.java
@@ -29,8 +29,9 @@ public class PalindromePartitioningII {
 		boolean[][] isPalindrome = new boolean[s.length() + 1][s.length() + 1];
 		int[] dp = new int[s.length() + 1];
 		dp[0] = -1;
+		dp[1] = 0;
 
-		for (int i = 1; i < dp.length; i++) {
+		for (int i = 2; i < dp.length; i++) {
 			dp[i] = i - 1;
 			for (int j = 1; j <= i; j++) {
 				// little trick here

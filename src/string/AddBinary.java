c270cec2dbe5f07501d5066f8634caab7364a031
leoleong <renzhejiajia@qq.com>
2014-08-25 21:28:06 +0800

    Update 'Add Binary'. - Add problem description.

--- src/string/AddBinary.java
+++ src/string/AddBinary.java
@@ -1,3 +1,12 @@
+/**
+ * Problem:
+ * Given two binary strings, return their sum (also a binary string).
+ * 
+ * For example,
+ * 	a = "11"
+ * 	b = "1"
+ * Return "100".
+ */
 package string;
 
 public class AddBinary {
@@ -14,34 +23,36 @@ public class AddBinary {
 
 	public static String addBinary(String a, String b) {
 
-		int aindex = a.length() - 1;
-		int bindex = b.length() - 1;
-		int ach, bch;
+		char[] array1 = a.toCharArray();
+		char[] array2 = b.toCharArray();
+		int idx1 = array1.length - 1;
+		int idx2 = array2.length - 1;
+		int op1, op2;
 		int carry = 0;
 		String s = "";
 
-		while (aindex >= 0 && bindex >= 0) {
-			ach = a.charAt(aindex) - '0';
-			bch = b.charAt(bindex) - '0';
-			s = (ach + bch + carry) % 2 + s;
-			carry = (ach + bch + carry) / 2;
-			aindex--;
-			bindex--;
+		while (idx1 >= 0 && idx2 >= 0) {
+			op1 = array1[idx1] - '0';
+			op2 = array2[idx2] - '0';
+			s = (op1 + op2 + carry) % 2 + s;
+			carry = (op1 + op2 + carry) / 2;
+			idx1--;
+			idx2--;
 		}
-		while (aindex >= 0) {
-			ach = a.charAt(aindex) - '0';
-			s = (ach + carry) % 2 + s;
-			carry = (ach + carry) / 2;
-			aindex--;
+		while (idx1 >= 0) {
+			op1 = array1[idx1] - '0';
+			s = (op1 + carry) % 2 + s;
+			carry = (op1 + carry) / 2;
+			idx1--;
 		}
-		while (bindex >= 0) {
-			bch = b.charAt(bindex) - '0';
-			s = (bch + carry) % 2 + s;
-			carry = (bch + carry) / 2;
-			bindex--;
+		while (idx2 >= 0) {
+			op2 = array2[idx2] - '0';
+			s = (op2 + carry) % 2 + s;
+			carry = (op2 + carry) / 2;
+			idx2--;
 		}
 		if (carry == 1) {
-			s = '1' + s;
+			s = 1 + s;
 		}
 
 		return s;

5b85e6d5bd3b174e4231da53f2e18b92c162b954
leoleong <renzhejiajia@qq.com>
2014-08-31 15:51:33 +0800

    Update '3 Sum Closest'. - Add problem description.

--- src/array/_3SumClosest.java
+++ src/array/_3SumClosest.java
@@ -1,3 +1,11 @@
+/**
+ * Problem:
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. 
+ * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+ * 
+ * For example, given array S = {-1 2 1 -4}, and target = 1.
+ * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
 package array;
 
 import java.util.Arrays;
@@ -12,46 +20,34 @@ public class _3SumClosest {
 
 	public static int threeSumClosest(int[] num, int target) {
 
-		Arrays.sort(num);
-		int diff = Integer.MAX_VALUE;
-		int closest = 0;
-		int sum = 0;
-		boolean flag = false;
-
-		int i, j, k;
-		if (num.length <= 3) {
-			for (int n : num) {
-				closest += n;
-			}
-			return closest;
+		if (num == null || num.length < 3) {
+			return Integer.MAX_VALUE;
 		}
-		for (i = 0; i <= num.length - 3 && !flag; i++) {
+
+		Arrays.sort(num);
+		int result = num[0] + num[1] + num[2];
+		int sum = 0;
+
+		for (int i = 0; i <= num.length - 3; i++) {
 			if (i > 0 && num[i] == num[i - 1]) {
 				continue;
 			}
-			j = i + 1;
-			k = num.length - 1;
+			int j = i + 1;
+			int k = num.length - 1;
 			while (j < k) {
 				sum = num[i] + num[j] + num[k];
 				if (sum == target) {
-					closest = target;
-					flag = true;
-					break;
+					return target;
 				} else if (sum < target) {
-					if (target - sum < diff) {
-						diff = target - sum;
-						closest = sum;
-					}
 					j++;
 				} else {
-					if (sum - target < diff) {
-						diff = sum - target;
-						closest = sum;
-					}
 					k--;
 				}
+				result = Math.abs(sum - target) < Math.abs(result - target) ? sum
+						: result;
 			}
 		}
-		return closest;
+
+		return result;
 	}
 }

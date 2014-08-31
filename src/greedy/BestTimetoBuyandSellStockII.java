c7d7066b7fffd256737933858c1a2f2d33663a91
leoleong <renzhejiajia@qq.com>
2014-08-31 11:51:30 +0800

    Update 'Best Time to Buy and Sell Stock II'. - Add problem description.

--- src/greedy/BestTimetoBuyandSellStockII.java
+++ src/greedy/BestTimetoBuyandSellStockII.java
@@ -1,3 +1,11 @@
+/**
+ * Problem:
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like 
+ * (ie, buy one and sell one share of the stock multiple times). 
+ * However, you may not engage in multiple transactions at the same time 
+ * (ie, you must sell the stock before you buy again).
+ */
 package greedy;
 
 public class BestTimetoBuyandSellStockII {
@@ -7,6 +15,10 @@ public class BestTimetoBuyandSellStockII {
 	}
 
 	public static int maxProfit(int[] prices) {
+
+		if (prices == null || prices.length < 2) {
+			return 0;
+		}
 
 		// special&functional test case
 		int profit = 0;

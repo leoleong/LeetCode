d5fc85f782c102e9c260d7fa6a67666aaa484709
leoleong <renzhejiajia@qq.com>
2014-08-31 11:51:45 +0800

    Update 'Best Time to Buy and Sell Stock III'. - Add problem description.

--- src/greedy/BestTimetoBuyandSellStockIII.java
+++ src/greedy/BestTimetoBuyandSellStockIII.java
@@ -1,4 +1,12 @@
-package dp;
+/**
+ * Problem:
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * Design an algorithm to find the maximum profit. You may complete at most two transactions.
+ * 
+ * Note:
+ * You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ */
+package greedy;
 
 public class BestTimetoBuyandSellStockIII {
 
@@ -9,19 +17,17 @@ public class BestTimetoBuyandSellStockIII {
 		System.out.println(result);
 	}
 
-	// compare with BestTimetoBuyandSellStock
 	public static int maxProfit(int[] prices) {
 
-		int length = prices.length;
-
 		// special test case
-		if (length < 2) {
+		if (prices == null || prices.length < 2) {
 			return 0;
 		}
 
 		// functional test case
+		int length = prices.length;
 		int[] order = new int[length];
-		int[] reverse = new int[length];
+		int[] reorder = new int[length];
 		int valley = prices[0];
 		int peak = prices[length - 1];
 		int profit = 0;
@@ -31,11 +37,11 @@ public class BestTimetoBuyandSellStockIII {
 			valley = Math.min(prices[i], valley);
 		}
 		for (int i = length - 2; i >= 0; i--) {
-			reverse[i] = Math.max(reverse[i + 1], peak - prices[i]);
+			reorder[i] = Math.max(reorder[i + 1], peak - prices[i]);
 			peak = Math.max(prices[i], peak);
 		}
 		for (int i = 0; i < length; i++) {
-			profit = Math.max(profit, order[i] + reverse[i]);
+			profit = Math.max(profit, order[i] + reorder[i]);
 		}
 
 		return profit;

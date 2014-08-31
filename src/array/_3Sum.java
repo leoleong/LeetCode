529a5249db7fdf8f07996d24d3a3ffe8015ae36b
leoleong <renzhejiajia@qq.com>
2014-08-31 15:51:10 +0800

    Update '3 Sum'. - Add problem description.

--- src/array/_3Sum.java
+++ src/array/_3Sum.java
@@ -1,60 +1,87 @@
+/**
+ * Problem:
+ * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? 
+ * Find all unique triplets in the array which gives the sum of zero.
+ * 
+ * Note:
+ * 	- Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+ * 	- The solution set must not contain duplicate triplets.
+ * 
+ * For example, given array S = {-1 0 1 2 -1 -4},
+ * A solution set is:
+ * (-1, 0, 1)
+ * (-1, -1, 2)
+ */
 package array;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class _3Sum {
 
 	public static void main(String[] args) {
 
 		int[] num = new int[] { 0, 0, 0 };
-		System.out.println(threeSum(num));
+		List<List<Integer>> result = threeSum(num);
+		System.out.println(result);
 	}
 
-	public static ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+	// Method 1: 2 pointer
+	public static List<List<Integer>> threeSum(int[] num) {
 
-		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+		if (num == null || num.length < 3) {
+			return result;
+		}
+
 		Arrays.sort(num);
-		int i, j, k;
-		for (i = 0; i <= num.length - 3 && num[i] <= 0; i++) {
+		for (int i = 0; i <= num.length - 3 && num[i] <= 0; i++) {
 			if (i > 0 && num[i] == num[i - 1]) {
 				continue;
 			}
-			j = i + 1;
-			k = num.length - 1;
+			int j = i + 1;
+			int k = num.length - 1;
 			while (j < k) {
-				if (num[i] + num[j] + num[k] == 0) {
-					if (j >= i + 2 && num[j] == num[j - 1]) {
-						j++;
-						k--;
-						continue;
-					}
-					ArrayList<Integer> tmpList = new ArrayList<Integer>();
-					tmpList.add(num[i]);
-					tmpList.add(num[j]);
-					tmpList.add(num[k]);
-					list.add(tmpList);
+				int sum = num[i] + num[j] + num[k];
+				if (sum == 0) {
+					ArrayList<Integer> list = new ArrayList<Integer>();
+					list.add(num[i]);
+					list.add(num[j]);
+					list.add(num[k]);
+					result.add(list);
 					j++;
 					k--;
-				} else if (num[i] + num[j] + num[k] < 0) {
+					while (j < k && num[j] == num[j - 1]) {
+						j++;
+					}
+					while (j < k && num[k] == num[k + 1]) {
+						k--;
+					}
+				} else if (sum < 0) {
 					j++;
 				} else {
 					k--;
 				}
 			}
 		}
-		return list;
+
+		return result;
 	}
 
+	// Method 2: HashMap
 	public static ArrayList<ArrayList<Integer>> hashThreeSum(int[] num) {
 
-		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
-		if (num.length < 3) {
-			return list;
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+		if (num == null || num.length < 3) {
+			return result;
 		}
 
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(num.length);
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(
+				num.length);
 		for (int i : num) {
 			if (map.containsKey(i)) {
 				int value = map.get(i) + 1;
@@ -65,10 +92,9 @@ public class _3Sum {
 		}
 
 		Arrays.sort(num);
-
 		boolean endFlag = false;
 		boolean case1, case2, case3;
-		ArrayList<Integer> tmpList = new ArrayList<Integer>(3);
+		ArrayList<Integer> list = new ArrayList<Integer>(3);
 		for (int i = 0; i < num.length; i++) {
 			if (endFlag) {
 				break;
@@ -84,21 +110,23 @@ public class _3Sum {
 					break;
 				}
 				if (map.containsKey(target)) {
-					case1 = (target == addend1 && target == addend2 && map.get(target) >= 3);
-					case2 = (target == addend1 || target == addend2) && map.get(target) >= 2;
+					case1 = (target == addend1 && target == addend2 && map
+							.get(target) >= 3);
+					case2 = (target == addend1 || target == addend2)
+							&& map.get(target) >= 2;
 					case3 = (target != addend1 && target != addend2);
 					if (case1 || case2 || case3) {
-						tmpList.clear();
-						tmpList.add(addend1);
-						tmpList.add(addend2);
-						tmpList.add(target);
-						if (!list.contains(tmpList)) {
-							list.add(tmpList);
+						list.clear();
+						list.add(addend1);
+						list.add(addend2);
+						list.add(target);
+						if (!result.contains(list)) {
+							result.add(list);
 						}
 					}
 				}
 			}
 		}
-		return list;
+		return result;
 	}
 }

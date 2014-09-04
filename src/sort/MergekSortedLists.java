469aa5b4a53cad46da232a300346ca0b97c99acd
leoleong <renzhejiajia@qq.com>
2014-09-04 09:10:54 +0800

    Update 'Merge K Sorted Lists'. - Add heapsort, O(n*logk).

--- src/sort/MergekSortedLists.java
+++ src/sort/MergekSortedLists.java
@@ -1,6 +1,13 @@
+/**
+ * Problem:
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ */
 package sort;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class MergekSortedLists {
 
@@ -8,7 +15,48 @@ public class MergekSortedLists {
 
 	}
 
+	// Method 1: O(N*logk)
 	public static ListNode mergeKLists(ArrayList<ListNode> lists) {
+
+		if (lists == null || lists.size() == 0) {
+			return null;
+		}
+
+		Queue<ListNode> heap = new PriorityQueue<>(lists.size(),
+				new Comparator<ListNode>() {
+					public int compare(ListNode node1, ListNode node2) {
+						if (node1 == null) {
+							return 1;
+						} else if (node2 == null) {
+							return -1;
+						}
+
+						return node1.val - node2.val;
+					}
+				});
+		
+		for (int i = 0; i < lists.size(); i++) {
+			if (lists.get(i) != null) {
+				heap.offer(lists.get(i));
+			}
+		}
+
+		ListNode dummy = new ListNode(-1);
+		ListNode cur = dummy;
+		while (!heap.isEmpty()) {
+			ListNode head = heap.poll();
+			cur.next = head;
+			cur = cur.next;
+			if (head.next != null) {
+				heap.offer(head.next);
+			}
+		}
+
+		return dummy.next;
+	}
+
+	// Method 2: Time > O(N*logk)
+	public static ListNode mergeKListsUsingMerge2(ArrayList<ListNode> lists) {
 
 		// special Test Case
 		if (lists.size() < 1) {
@@ -53,7 +101,6 @@ public class MergekSortedLists {
 
 		ListNode(int x) {
 			val = x;
-			next = null;
 		}
 	}
 }

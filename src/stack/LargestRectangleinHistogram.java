749a2f38e3cd61c6eb0dedbcb980ca66b76ae422
leoleong <renzhejiajia@qq.com>
2014-09-20 16:38:12 +0800

    Update problem 'Largest Rectangle in Histogram'.

--- src/stack/LargestRectangleinHistogram.java
+++ src/stack/LargestRectangleinHistogram.java
@@ -1,27 +1,33 @@
+/**
+ * Problem:
+ * Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, 
+ * find the area of largest rectangle in the histogram.
+ * Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+ * 
+ * For example,
+ * Given height = [2,1,5,6,2,3],
+ * return 10.
+ */
 package stack;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
 
-/**
- * author : leo
- * date : 2014-5-19
- * comment : 
- * stack维护非递减数列
- */
 public class LargestRectangleinHistogram {
 
 	public static void main(String[] args) {
 
-		int[] height = new int[] { 1 };
-
+		int[] height = new int[] { 2, 1, 5, 6, 2, 3 };
 		int result = largestRectangleArea(height);
-
 		System.out.println(result);
 	}
 
-	public static int largestRectangleArea(int[] height) {
+	private static int largestRectangleArea(int[] height) {
+
+		if (height == null || height.length == 0) {
+			return 0;
+		}
 
 		Deque<Integer> stack = new ArrayDeque<Integer>();
 		int[] h = Arrays.copyOf(height, height.length + 1);
@@ -32,7 +38,7 @@ public class LargestRectangleinHistogram {
 				stack.push(i++);
 			} else {
 				int index = stack.pop();
-				int width = stack.isEmpty() ? i : i - 1 - stack.peek();
+				int width = stack.isEmpty() ? i : (i - 1) - stack.peek();
 				max = Math.max(max, h[index] * width);
 			}
 		}

98212ef688718d5781c3bdb0e3f685fc93c7a77d
leoleong <renzhejiajia@qq.com>
2014-09-20 22:59:34 +0800

    Update problem 'Longest Valid Parentheses'.

--- src/stack/LongestValidParentheses.java
+++ src/stack/LongestValidParentheses.java
@@ -1,46 +1,45 @@
+/**
+ * Problem:
+ * Given a string containing just the characters '(' and ')', 
+ * find the length of the longest valid (well-formed) parentheses substring.
+ * For "(()", the longest valid parentheses substring is "()", which has length = 2.
+ * Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+ */
 package stack;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-/**
- * author : leo
- * date : 2014-5-19
- * comment : 
- * 体会4种点不同的作用
- */
 public class LongestValidParentheses {
 
 	public static void main(String[] args) {
 
 		String s = "()(()()";
-
 		int result = longestValidParentheses(s);
-
 		System.out.println(result);
 	}
 
 	public static int longestValidParentheses(String s) {
 
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+
 		Deque<Integer> stack = new ArrayDeque<Integer>();
-		int last = -1;
+		int start = 0;
 		int max = 0;
-		char ch;
 
 		for (int i = 0; i < s.length(); i++) {
-			ch = s.charAt(i);
+			char ch = s.charAt(i);
 			if (ch == '(') {
 				stack.push(i);
 			} else {
 				if (stack.isEmpty()) {
-					last = i;
+					start = i + 1;
 				} else {
 					stack.pop();
-					if (stack.isEmpty()) {
-						max = Math.max(max, i - last);
-					} else {
-						max = Math.max(max, i - stack.peek());
-					}
+					max = stack.isEmpty() ? Math.max(max, i - start + 1) : Math
+							.max(max, i - stack.peek());
 				}
 			}
 		}

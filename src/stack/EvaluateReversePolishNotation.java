43678bab5100ec2a2b766da60ae8b7364bb86784
leoleong <renzhejiajia@qq.com>
2014-09-21 10:20:36 +0800

    Update problem 'Evaluate Reverse Polish Notation'.

--- src/stack/EvaluateReversePolishNotation.java
+++ src/stack/EvaluateReversePolishNotation.java
@@ -1,3 +1,12 @@
+/**
+ * Problem:
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+ * 
+ * Some examples:
+ * 	["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ * 	["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ */
 package stack;
 
 import java.util.ArrayDeque;
@@ -8,16 +17,14 @@ public class EvaluateReversePolishNotation {
 	public static void main(String[] args) {
 
 		String[] tokens = new String[] { "4", "3", "-" };
-
 		int result = evalRPN(tokens);
-
 		System.out.println(result);
 	}
 
 	public static int evalRPN(String[] tokens) {
 
-		int oprand;
 		Deque<Integer> stack = new ArrayDeque<Integer>();
+		int oprand;
 
 		for (String token : tokens) {
 			if (token.equals("+")) {

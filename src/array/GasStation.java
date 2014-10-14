8485cff489e246c9d85b320732adf3aa45609c39
leo leong <340623792@qq.com>
2014-10-14 17:11:01 +0800

    Update problem 'Gas Station'. - Add problem description.

--- src/array/GasStation.java
+++ src/array/GasStation.java
@@ -1,3 +1,13 @@
+/**
+ * Problem:
+ * There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+ * You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). 
+ * You begin the journey with an empty tank at one of the gas stations.
+ * Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+ * 
+ * Note:
+ * The solution is guaranteed to be unique.
+ */
 package array;
 
 public class GasStation {
@@ -10,15 +20,17 @@ public class GasStation {
 
 		int total = 0;
 		int sum = 0;
-		int j = -1;
+		int pos = -1;
+
 		for (int i = 0; i < cost.length; i++) {
 			sum += gas[i] - cost[i];
 			total += gas[i] - cost[i];
 			if (sum < 0) {
 				sum = 0;
-				j = i;
+				pos = i;
 			}
 		}
-		return (total < 0) ? -1 : (j + 1);
+
+		return (total < 0) ? -1 : (pos + 1);
 	}
 }
